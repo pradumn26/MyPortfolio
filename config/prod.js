0199f89ca1087488f6ff4a9e34bb5868b8513737
pradumn26 <pradumn.26@gmail.com>
2018-04-05 00:55:58 +0530

    home and contact complete

---
+++ config/prod.js
@@ -0,0 +1,3 @@
+exports = module.exports = {
+    sendgridKey: process.env.SENDGRID_KEY
+};
--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const path = require('path');
+const sgMail = require('@sendgrid/mail');
+const keys = require('./config/keys');
+
+sgMail.setApiKey(keys.sendgridKey);
 
 var app = express();
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
    res.sendFile(path.resolve(__dirname, 'frontend', 'index.html'));
+});
+
+app.post('/api/send_mail', (req, res) => {
+    let mail = req.body;
+
+    const msg = {
+        to: 'pradumn.26@gmail.com',
+        from: mail.email,
+        subject: mail.subject,
+        text: mail.message
+    };
+    sgMail.send(msg);
+
+    res.redirect('/');
 });
 
 let PORT = process.env.PORT || 4000;
